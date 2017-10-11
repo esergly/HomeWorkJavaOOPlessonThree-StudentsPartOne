1f853a9024480f30cd6b72325fdb27076fb2efdf
esergly <kievbsm@gmail.com>
2017-10-11 19:22:40 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,33 +1,27 @@
 public class Main {
     public static void main(String[] args) {
 
-        // The list of people who wants to be a students.
+        // The list of people for test Task one.
         Human peopleOne = new Human("John", "Bell", 21, "male", 181, 75.6);
         Human peopleTwo = new Human("Jane", "Carroll", 18, "female", 170, 48.7);
         Human peopleThree = new Human("Anna", "Smith", 19, "female", 174, 50.0);
-        Human peopleFour = new Human("Nick", "Tompson", 22, "male", 178, 72.5);
-        Human peopleFive = new Human("Kate", "Blange", 20, "female", 168, 78.2);
-        Human peopleSix = new Human("Ben", "Bull", 17, "male", 181, 70.1);
-        Human peopleSeven = new Human("Chris", "Peterson", 21, "male", 190, 92.8);
-        Human peopleEight = new Human("Diane", "Holsson", 20, "female", 172, 46.3);
-        Human peopleNine = new Human("George", "Klarkson", 17, "male", 178, 80.8);
-        Human peopleTen = new Human("Pitt", "Erwin", 18, "male", 181, 85.1);
-        Human peopleEleven = new Human("Paula", "Kotlin", 22, "female", 171, 47.3);
+        System.out.println(peopleOne);
+        System.out.println(peopleThree);
 
          Group genius = new Group(); // Create the new group of students
 
-        //Change status for people who have become students.
-        Student studentOne = new Student(peopleOne, "Finance", 5);
-        Student studentTwo = new Student(peopleTwo, "Mathematic", 2);
-        Student studentThree = new Student(peopleThree, "Logic", 3);
-        Student studentFour = new Student(peopleFour, "Politic", 5);
-        Student studentFive = new Student(peopleFive, "Finance", 5);
-        Student studentSix = new Student(peopleSix, "Mathematic", 1);
-        Student studentSeven = new Student(peopleSeven, "Logic", 5);
-        Student studentEight = new Student(peopleEight, "Politic", 2);
-        Student studentNine = new Student(peopleNine, "Mathematic", 4);
-        Student studentTen = new Student(peopleTen, "Logic", 5);
-        Student studentEleven = new Student(peopleEleven, "Politic", 3);//for reject of addition action test
+        //The list of students for test Task two.
+        Student studentOne = new Student("John", "Bell", 21, "male", 181, 75.6,"Finance", 5);
+        Student studentTwo = new Student("Jane", "Carroll", 18, "female", 170, 48.7,"Mathematic", 2);
+        Student studentThree = new Student("Anna", "Smith", 19, "female", 174, 50.0,"Logic", 3);
+        Student studentFour = new Student("Nick", "Tompson", 22, "male", 178, 72.5,"Politic", 5);
+        Student studentFive = new Student("Kate", "Blange", 20, "female", 168, 78.2,"Finance", 5);
+        Student studentSix = new Student("Ben", "Bull", 17, "male", 181, 70.1,"Mathematic", 1);
+        Student studentSeven = new Student("Chris", "Peterson", 21, "male", 190, 92.8,"Logic", 5);
+        Student studentEight = new Student("Diane", "Holsson", 20, "female", 172, 46.3,"Politic", 2);
+        Student studentNine = new Student("George", "Klarkson", 17, "male", 178, 80.8,"Mathematic", 4);
+        Student studentTen = new Student("Pitt", "Erwin", 18, "male", 181, 85.1,"Logic", 5);
+        Student studentEleven = new Student("Paula", "Kotlin", 22, "female", 171, 47.3,"Politic", 3);//for reject of addition action test
 
       //Try to add new members into the student's group.
         try {
@@ -41,6 +35,8 @@ public class Main {
             genius.addStudentToTheGroup(studentEight);
             genius.addStudentToTheGroup(studentNine);
             genius.addStudentToTheGroup(studentTen);
+//            System.out.println(genius); Проверить засаду: меняются индексы в цикле метода поиска по фамилии 
+//            (похоже на слабый комп)
 
             //   Now will try to add a new member to the group where no more free spaces.
             genius.addStudentToTheGroup(studentEleven);
@@ -54,7 +50,6 @@ public class Main {
 
         //Try to remove bad students from the student's group.
         genius.removeStudentFromTheGroup(4);
-        System.out.println(genius);
 
     }
 }
